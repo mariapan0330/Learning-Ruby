bfc8a8e52c8f7e417a6776f3d81bbf948771beea
Maria Panagos <maria.pan0330@gmail.com>
2023-04-23 16:45:25 -0500

    prints and variable types with Ruby

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,18 @@
+# remember to save before running
 puts "Hello World from Ruby"
+
+my_num = 25 # snake_case
+my_bool = true # lowercase booleans
+my_string = "Maria" # dynamically typed
+my_num = 25 % 6
+
+puts my_string
+puts my_num
+
+# puts adds newline at the end of a statement
+# print has no newline
+puts "Put this down"
+print "Print this"
+print " and this"
+puts "This is not a new line"
+puts 'but this is'
